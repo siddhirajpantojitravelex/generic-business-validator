eb7a98521823a8a22172024a094720d173a20624
Pantoji, Siddhiraj <siddhiraj.pantoji@travelex.com>
2019-06-05 17:03:21 +0530

    Changes for External Log Config

--- api-operations/business-validator/log-config.js
+++ api-operations/business-validator/log-config.js
@@ -1,38 +1,16 @@
-// log-config.js
 var logTypes = require('winston-wrapper/log-type')
 module.exports = {
-    pattern: " ext [${label}] ${timestamp} [Co-relation-id : ${traceID}] [${level}]: ${message} ",
-    appenders: [
-        {
-            // Console 
-            type: logTypes.Console,
-            options: {
-                level: 'debug',
-                handleExceptions: true
-            }
-        },
-        {
-            // File - Rolling File appender  
-            type: logTypes.File,
-            options: {
-                level: 'info',
-                filename: 'app.log',
-                handleExceptions: true,
-                maxsize: 5242880, // 5MB
-                maxFiles: 5,
-
-            }
-        },
-        {
-            // Only File based on error Level 
-            type: logTypes.File,
-            options: {
-                level: 'error',
-                filename: 'error.log',
-                handleExceptions: true,
-                json: true,
-                colorize: false,
-            }
+    pattern : " ext [${label}] ${timestamp} [Co-relation-id : ${correlationid}] [${level}]: ${message} ",
+    datetimePattern : "YYYY-MM-DD HH:mm:ss",
+    appenders : [
+      {
+        type:logTypes.Console,
+        options : {
+          level: 'debug',
+          handleExceptions: true,
+          json: false,
+          colorize: true,
         }
+      }
     ]
-}
+  }
